a45515333bc3fdb29742a2af2ef2bc2c4f77eb1b
Juanito-boop <acevedojuanesteban.colombia@gmail.com>
2024-06-15 14:44:48 -0500

    chore: Remove commented-out code from MainHeader component

--- app/ui/home/main-header.tsx
+++ app/ui/home/main-header.tsx
@@ -27,26 +27,6 @@ export default function MainHeader() {
 							 id="icono-menu" alt="" className="w-10 align-top cursor-pointer" />
 					 </div>
 				 </div>
-				 {/* <form id="myForm" action="" method="POST" className="inline-flex items-center w-full h-10 mx-auto justify-evenly">
-					 <div className="flex items-center justify-center w-screen p-5">
-						 <div className="w-full p-5 rounded-lg">
-							 <div className="flex">
-								 <div
-									 className="flex items-center justify-center w-10 p-5 border-r rounded-tl-lg rounded-bl-lg border-violet-500 bg-violet-200">
-									 <svg viewBox="0 0 20 20" aria-hidden="true" className="absolute w-5 transition pointer-events-none fill-black">
-										 <path
-											 d="M16.72 17.78a.75.75 0 1 0 1.06-1.06l-1.06 1.06ZM9 14.5A5.5 5.5 0 0 1 3.5 9H2a7 7 0 0 0 7 7v-1.5ZM3.5 9A5.5 5.5 0 0 1 9 3.5V2a7 7 0 0 0-7 7h1.5ZM9 3.5A5.5 5.5 0 0 1 14.5 9H16a7 7 0 0 0-7-7v1.5Zm3.89 10.45 3.83 3.83 1.06-1.06-3.83-3.83-1.06 1.06ZM14.5 9a5.48 5.48 0 0 1-1.61 3.89l1.06 1.06A6.98 6.98 0 0 0 16 9h-1.5Zm-1.61 3.89A5.48 5.48 0 0 1 9 14.5V16a6.98 6.98 0 0 0 4.95-2.05l-1.06-1.06Z">
-										 </path>
-									 </svg>
-								 </div>
-								 <input type="text" className="w-full pl-2 text-base font-semibold bg-white border-violet-500 outline-0"
-									 placeholder="" id="" disabled />
-								 <input type="button" value="Buscar"
-									 className="p-2 font-semibold text-white transition-colors rounded-tr-lg rounded-br-lg bg-violet-600 hover:bg-violet-800" />
-							 </div>
-						 </div>
-					 </div>
-				 </form> */}
 			 </header>
 			 <script src="https://npuxpuelimayqrsmzqur.supabase.co/storage/v1/object/public/images/js/menu.js"></script>
 		 </>
