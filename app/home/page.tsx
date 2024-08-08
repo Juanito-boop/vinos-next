883edc35d11e935c28e51ef7d21e3298953a6c2f
Juanito-boop <84400264+Juanito-boop@users.noreply.github.com>
2024-08-08 00:12:14 +0000

    refactor: Remove unused code and update component imports in home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,5 @@
 import MainHeader from "@/app/ui/home/main-header";
 import AsideMainServer from "@/app/ui/home/servidor/AsideMainServer";
-import CardsMainClientWrapper from "@/app/ui/home/CardsMainClientWrapper";
-import CardsMain from "@/app/ui/home/servidor/CardsMain";
 import FilteredCards from "@/app/ui/home/FilterCardProducts";
 
 export default function Page() {
@@ -10,7 +8,6 @@ export default function Page() {
 			<MainHeader />
 			<section className="flex flex-row mx-2.5 gap-2">
 				<AsideMainServer />
-				{/* <CardsMain /> */}
 				<FilteredCards />
 			</section>
 		</>
--- app/ui/home/FilterCardProducts.tsx
+++ app/ui/home/FilterCardProducts.tsx
@@ -1,18 +1,40 @@
 "use client"
 
 import { useEffect, useState, useCallback } from 'react';
+// import { useLocation } from 'react-router-dom';
 
 export default function FilteredCards() {
-  const [variedades, setVariedades] = useState('');
-  useEffect(() => {
-    
-  }, []);
+  // leer los parametros de la url y actualizar el estado del componente
+  // const [variedadesArray, setVariedadesArray] = useState<string[]>([]);
+  // const location = useLocation();
+
+  // const checkVariedades = () => {
+  //   const urlParams = new URLSearchParams(location.search);
+  //   const variedadesValue = urlParams.get('variedades');
+
+  //   if (variedadesValue) {
+  //     const nuevasVariedades = variedadesValue.split(','); // Separar por comas
+  //     setVariedadesArray([...new Set(nuevasVariedades)]);
+  //   } else {
+  //     setVariedadesArray([]);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   checkVariedades();
+  // }, [location.search]);
 
   return (
     <>
-      <main className="w-full px-1 pt-3 pb-1 bg-normalColor11 rounded-r-xl">
-        {JSON.stringify(variedades) || 'No hay variedades'}
-      </main>
+      {/* {variedadesArray.length > 0 ? (
+        <main className="w-full px-1 pt-3 pb-1 bg-normalColor11 rounded-r-xl">
+          <p>Variedades seleccionadas: {variedadesArray.join(', ')}</p>
+        </main>
+      ) : (
+        <main className="w-full px-1 pt-3 pb-1 bg-normalColor11 rounded-r-xl">
+          <p>No hay variedades seleccionadas</p>
+        </main>
+      )} */}
     </>
   );
 }
